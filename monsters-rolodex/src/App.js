26bcf3b679b4fc49e391da31e5f6d36910718bdf
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-13 19:00:28 -0500

    map functions

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,18 +8,37 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Youssef",
+      monsters: [
+        {
+          name: "Linda",
+          id: "1",
+        },
+        {
+          name: "Frank",
+          id: "2",
+        },
+        {
+          name: "Jacky",
+          id: "3",
+        },
+        {
+          name: "Kira",
+          id: "4",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Cahnge Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
