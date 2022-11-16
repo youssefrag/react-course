942caedc3fe5bea0e3081293c48c74f4e99e7f51
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-16 13:14:04 -0500

    extract card into seperate component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Roledex</h1>
         <SearchBox
           className="monsters-search-box"
           onChangeHandler={onSearchChange}
--- monsters-rolodex/src/components/CardList/CardList.jsx
+++ monsters-rolodex/src/components/CardList/CardList.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 
+import Card from "../Card/Card";
+
+import "./CardList.css";
+
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
     return (
       <div className="card-list">
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        {monsters.map((monster) => {
+          return <Card monster={monster} key={monster.id} />;
+        })}
       </div>
     );
   }
