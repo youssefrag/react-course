976241d813d56e77fd8a135f0fa072fe087fa252
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-16 12:32:48 -0500

    css in react

--- monsters-rolodex/src/components/CardList/CardList.jsx
+++ monsters-rolodex/src/components/CardList/CardList.jsx
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 
 class CardList extends Component {
   render() {
-    console.log(this.props.monsters);
-    console.log("render from CardList");
     const { monsters } = this.props;
 
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
