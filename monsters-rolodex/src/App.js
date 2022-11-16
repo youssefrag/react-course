14de9501e861a906b06753dab5f8013823193726
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-16 18:30:27 -0500

    basic use state

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,19 +1,27 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import CardList from "./components/CardList/CardList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 
 const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log({ searchField });
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Roledex</h1>
-      {/* <SearchBox
+      <SearchBox
         className="monsters-search-box"
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
       />
-      <CardList monsters={filteredMonsters} /> */}
+      {/* <CardList monsters={filteredMonsters} /> */}
     </div>
   );
 };
