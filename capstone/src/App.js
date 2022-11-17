bfe189b52eca1fb4b03d3d13570d5357e6250ee3
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-17 17:24:16 -0500

    link component

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => <h1>I am the shop page</h1>;
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
