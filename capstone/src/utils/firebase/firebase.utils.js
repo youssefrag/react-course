a5b67ed711e12fa3edb8112dec964dbefa302036
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-18 11:49:41 -0500

    set up user documents

--- capstone/src/utils/firebase/firebase.utils.js
+++ capstone/src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyACQ8QKSWAvZfYXmNgFhuEdKx4-xpSA7Zw",
@@ -25,3 +26,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
