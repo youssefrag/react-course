517de89546a2c7cd21fc4e1e3fcdb2126777dd2d
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-18 13:28:11 -0500

    finish building sing up form

--- capstone/src/routes/sign-in/sign-in.component.jsx
+++ capstone/src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,11 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
+
 const SignIn = () => {
-  useEffect(() => {
-    async function _getRedirectResult() {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    }
-
-    _getRedirectResult();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -29,9 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
+      <SignUpForm />
     </div>
   );
 };
