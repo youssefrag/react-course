eb747799c3bf38c275c17abdf55610c4cbdaffea
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-20 15:53:12 -0500

    complete authentication page

--- capstone/src/components/sign-up-form/sign-up-form.components.jsx
+++ capstone/src/components/sign-up-form/sign-up-form.components.jsx
@@ -56,7 +56,7 @@ const SignUpForm = () => {
 
   return (
     <div className="sign-up-container">
-      <h2>Don't have ann account?</h2>
+      <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
--- capstone/src/routes/authentication/authentication.component.jsx
+++ capstone/src/routes/authentication/authentication.component.jsx
@@ -1,20 +1,12 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
-
 import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
-
 import SignInForm from "../../components/sign-in-form/sign-in-form.components";
 
-import Button from "../../components/button/button.component";
+import "./authentication.styles.scss";
 
 const Authentication = () => {
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="authentication-container">
       <SignInForm />
-
       <SignUpForm />
     </div>
   );
