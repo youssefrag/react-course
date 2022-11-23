2e4110aaf69fd3655c7fef4a6752cbe6971c004f
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-23 10:39:07 -0500

    categories preview

--- capstone/src/routes/shop/shop.component.jsx
+++ capstone/src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 import "./shop.styles.scss";
 
@@ -9,18 +9,15 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
+    <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => (
-        <div key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
+        <CategoryPreview
+          key={title}
+          title={title}
+          products={categoriesMap[title]}
+        />
       ))}
-    </>
+    </div>
   );
 };
 
