6499f34153f9271c7279ca9626a2d2bf255b007f
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-23 16:17:54 -0500

    styled components for routes

--- capstone/src/routes/authentication/authentication.component.jsx
+++ capstone/src/routes/authentication/authentication.component.jsx
@@ -1,14 +1,14 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
 import SignInForm from "../../components/sign-in-form/sign-in-form.components";
 
-import "./authentication.styles.scss";
+import { AuthenticationContainer } from "./authentication.styles";
 
 const Authentication = () => {
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
