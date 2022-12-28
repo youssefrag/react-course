293cab191e918a4e1f4be04543cfb23d01ab11bb
Youssef Ragab <ragab.youssef95@gmail.com>
2022-12-28 19:56:47 +0200

    set current user redux

--- capstone/src/index.js
+++ capstone/src/index.js
@@ -16,13 +16,13 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        {/* <UserProvider> */}
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+        {/* </UserProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
