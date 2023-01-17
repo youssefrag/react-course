d06affede1819dcbfaff57ea10e0aaa56db317ae
Youssef Ragab <ragab.youssef95@gmail.com>
2023-01-17 21:24:23 +0200

    categories selector

--- capstone/src/utils/firebase/firebase.utils.js
+++ capstone/src/utils/firebase/firebase.utils.js
@@ -68,14 +68,6 @@ export const getCategoriesAndDocuments = async () => {
   const querySnapshot = await getDocs(q);
 
   return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
-
-  // .reduce((acc, docSnapshot) => {
-  //   const { title, items } = docSnapshot.data();
-  //   acc[title.toLowerCase()] = items;
-  //   return acc;
-  // }, {});
-
-  // return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
