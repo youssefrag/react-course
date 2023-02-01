63b30107ba3638f9d30ae33638ee166b4cc25eb7
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-01 17:47:29 -0500

    add cart to root reducer

---
+++ capstone/src/store/cart/cart.reducer.js
@@ -0,0 +1,22 @@
+import { CART_ACTION_TYPES } from "./cart.types";
+
+export const CART_INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    default:
+      return state;
+  }
+};
