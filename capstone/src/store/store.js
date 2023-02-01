3afc3003095411ede191d8a5e26d1a4bb8613f61
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-01 17:32:19 -0500

    memoize

--- capstone/src/store/store.js
+++ capstone/src/store/store.js
@@ -1,25 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from 'redux-logger';
 
 import { rootReducer } from "./root-reducer";
 
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
 
-//   console.log("type", action.type);
-//   console.log("payload", action.payload);
-//   console.log("currentState", store.getState());
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
 
-//   next(action);
+  next(action);
 
-//   console.log("next state: ", store.getState());
-// };
+  console.log("next state: ", store.getState());
+};
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
