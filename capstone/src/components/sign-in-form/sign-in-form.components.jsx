f1d555c75d9d8831d874876bb653f65bef8adab4
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-13 15:12:41 -0500

    sign in with user and password success

--- capstone/src/components/sign-in-form/sign-in-form.components.jsx
+++ capstone/src/components/sign-in-form/sign-in-form.components.jsx
@@ -4,13 +4,14 @@ import { useDispatch } from "react-redux";
 import FormInput from "./../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import { signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
-
 import {
   SignInContainer,
   ButtonsContainer,
 } from "./sign-in-form.styles.styles";
-import { googleSignInStart } from "../../store/user/user.action";
+import {
+  googleSignInStart,
+  emailSignInStart,
+} from "../../store/user/user.action";
 
 const defaultFormFields = {
   email: "",
@@ -35,7 +36,7 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      await signInAuthUserWithEmailAndPassword(email, password);
+      dispatch(emailSignInStart(email, password));
       resetFormFields();
     } catch (error) {
       console.log("user sign in failed", error);
