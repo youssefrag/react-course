9e780b1c66fae0bcc59cf2964ab5d683b5d1ce89
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-16 18:53:22 -0500

    introduce redux toolkit

--- capstone/src/store/store.js
+++ capstone/src/store/store.js
@@ -1,42 +1,41 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { compose, createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import createSagaMiddleware from "redux-saga";
+// import createSagaMiddleware from "redux-saga";
 
-import { rootSaga } from "../store/root-saga";
+// import { rootSaga } from "../store/root-saga";
 
 import { rootReducer } from "./root-reducer";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["cart"],
+// };
 
-const sagaMiddleWare = createSagaMiddleware();
+// const sagaMiddleWare = createSagaMiddleware();
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [
-  process.env.NODE_ENV !== "production" && logger,
-  sagaMiddleWare,
-].filter(Boolean);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
 );
 
-sagaMiddleWare.run(rootSaga);
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-export const persistor = persistStore(store);
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = configureStore({
+  reducer: rootReducer,
+  // middleware: middleWares,
+});
+
+// sagaMiddleWare.run(rootSaga);
+
+// export const persistor = persistStore(store);
