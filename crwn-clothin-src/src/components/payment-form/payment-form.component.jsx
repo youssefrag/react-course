a1209d12b0008b75e13f461eb8ccae9656c3765e
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-20 18:04:38 -0500

    button spinner

--- crwn-clothin-src/src/components/payment-form/payment-form.component.jsx
+++ crwn-clothin-src/src/components/payment-form/payment-form.component.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
@@ -7,6 +12,11 @@ import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const amount = useSelector(selectCartTotal);
+  const currentUser = useSelector(selectCurrentUser);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+
+  console.log(currentUser);
 
   const paymentHandler = async (e) => {
     e.preventDefault();
@@ -15,28 +25,30 @@ const PaymentForm = () => {
       return;
     }
 
+    setIsProcessingPayment(true);
+
     const response = await fetch("/.netlify/functions/create-payment-intent", {
       method: "post",
       header: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ amount: 10000 }),
+      body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => res.json());
 
     const {
       paymentIntent: { client_secret },
     } = response;
 
-    console.log(client_secret);
-
     const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: "Youssef Ragab",
+          name: currentUser ? currentUser.displayName : "Guest",
         },
       },
     });
+
+    setIsProcessingPayment(false);
 
     if (paymentResult.error) {
       alert(paymentResult.error);
@@ -52,7 +64,12 @@ const PaymentForm = () => {
       <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+        <Button
+          isLoading={isProcessingPayment}
+          buttonType={BUTTON_TYPE_CLASSES.inverted}
+        >
+          Pay now
+        </Button>
       </FormContainer>
     </PaymentFormContainer>
   );
---
+++ crwn-clothin-src/src/components/spinner/spinner.component.jsx
@@ -0,0 +1,11 @@
+import { SpinnerOverlay, SpinnerContainer } from "./spinner.styles";
+
+import React from "react";
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer></SpinnerContainer>
+  </SpinnerOverlay>
+);
+
+export default Spinner;
