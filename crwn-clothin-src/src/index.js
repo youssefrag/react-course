0cc42b1695043745a0f49669d6c79c5f6a5c31ef
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-20 10:58:57 -0500

    setup stripe

--- crwn-clothin-src/src/index.js
+++ crwn-clothin-src/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
-import { store, persistor } from "./store/store";
+import { store } from "./store/store";
+import { stripePromise } from "./utils/stripe/striple.utils";
 
 import "./index.scss";
 
@@ -14,11 +15,11 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <BrowserRouter>
-        <App />
+        <Elements striple={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   rootElement
