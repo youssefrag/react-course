fda7797d03a2c50ef5a32c244ab0f3cf9ae4f618
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-21 13:53:09 -0500

    clean up repo

--- capstone/src/routes/authentication/authentication.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const AuthenticationContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 900px;
-  margin: 30px auto;
-`;
