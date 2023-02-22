55ad1a243b94fd3895093ca9ab9bc358051882ad
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-22 15:31:32 -0500

    type out user store

--- ts-start-point/src/store/cart/cart.selector.ts
+++ ts-start-point/src/store/cart/cart.selector.ts
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 
 import { CartState } from "./cart.reducer";
 
-const selectCartReducer = (state: any): CartState => state.cart;
+const selectCartReducer = (state): CartState => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
