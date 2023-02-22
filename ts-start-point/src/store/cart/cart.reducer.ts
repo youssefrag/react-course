ccd118274639bcee36282fdf6c13cac0feabb2c1
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-22 12:48:36 -0500

    type cart selectors

---
+++ ts-start-point/src/store/cart/cart.reducer.ts
@@ -0,0 +1,29 @@
+import { AnyAction } from "redux";
+
+import { CartItem } from "./cart.types";
+import { setCartItems, setIsCartOpen } from "./cart.action";
+
+export type CartState = {
+  readonly isCartOpen: boolean;
+  readonly cartItems: CartItem[];
+};
+
+export const CART_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (
+  state = CART_INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setCartItems.match(action)) {
+    return { ...state, cartItems: action.payload };
+  }
+
+  if (setIsCartOpen.match(action)) {
+    return { ...state, isCartOpen: action.payload };
+  }
+
+  return state;
+};
