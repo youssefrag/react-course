23641f90cecc948938ea61cb29f17731176fc0df
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-23 17:35:42 -0500

    typing stripe payment

--- ts-start-point/src/components/payment-form/payment-form.component.tsx
+++ ts-start-point/src/components/payment-form/payment-form.component.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { useSelector } from 'react-redux';
+import { useState, FormEvent } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { StripeCardElement } from "@stripe/stripe-js";
+import { useSelector } from "react-redux";
 
-import { selectCartTotal } from '../../store/cart/cart.selector';
-import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { FormContainer } from './payment-form.styles';
-import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { FormContainer } from "./payment-form.styles";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
+import { PaymentButton, PaymentFormContainer } from "./payment-form.styles";
+
+const ifValidCardElement = (
+  card: StripeCardElement | null
+): card is StripeCardElement => card !== null;
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -17,16 +22,16 @@ const PaymentForm = () => {
   const currentUser = useSelector(selectCurrentUser);
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
-  const paymentHandler = async (e) => {
+  const paymentHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
     setIsProcessingPayment(true);
-    const response = await fetch('/.netlify/functions/create-payment-intent', {
-      method: 'post',
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => {
@@ -35,11 +40,15 @@ const PaymentForm = () => {
 
     const clientSecret = response.paymentIntent.client_secret;
 
+    const cardDetails = elements.getElement(CardElement);
+
+    if (!ifValidCardElement(cardDetails)) return;
+
     const paymentResult = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        card: cardDetails,
         billing_details: {
-          name: currentUser ? currentUser.displayName : 'Yihua Zhang',
+          name: currentUser ? currentUser.displayName : "Yihua Zhang",
         },
       },
     });
@@ -49,8 +58,8 @@ const PaymentForm = () => {
     if (paymentResult.error) {
       alert(paymentResult.error.message);
     } else {
-      if (paymentResult.paymentIntent.status === 'succeeded') {
-        alert('Payment Successful!');
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful!");
       }
     }
   };
