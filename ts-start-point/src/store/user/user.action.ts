e0164b6b641d8192622c7c73ee10e3111deac6d0
Youssef Ragab <ragab.youssef95@gmail.com>
2023-02-23 14:06:39 -0500

    typing all sagas

--- ts-start-point/src/store/user/user.action.ts
+++ ts-start-point/src/store/user/user.action.ts
@@ -1,3 +1,4 @@
+import { User } from "firebase/auth";
 import { USER_ACTION_TYPES } from "./user.types";
 
 import {
@@ -43,7 +44,7 @@ export type SignUpStart = ActionWithPayload<
 
 export type SignUpSuccess = ActionWithPayload<
   USER_ACTION_TYPES.SIGN_UP_SUCCESS,
-  { user: UserData; additionalDetails: AdditionalInformation }
+  { user: User; additionalDetails: AdditionalInformation }
 >;
 
 export type SignUpFailed = ActionWithPayload<
@@ -79,7 +80,7 @@ export const emailSignInStart = withMatcher(
 );
 
 export const signInSuccess = withMatcher(
-  (user: UserData): SignInSuccess =>
+  (user: UserData & { id: string }): SignInSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
@@ -98,7 +99,7 @@ export const signUpStart = withMatcher(
 );
 
 export const signUpSuccess = withMatcher(
-  (user: UserData, additionalDetails: AdditionalInformation): SignUpSuccess =>
+  (user: User, additionalDetails: AdditionalInformation): SignUpSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails })
 );
 
